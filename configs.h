0a0a5ac9849f53b3c3b0f512de388f26fae96d3f
Yang Minshun <yangminshun@abestswc.com>
2020-07-21 20:35:14 +0900

    New config value

--- configs.h
+++ configs.h
@@ -10,6 +10,7 @@ extern double CHECK_POINT_SENSITIVITY;  // Min: 0; Max: 1
 extern int LENGTH_OF_MAP_BLOCK;
 extern int MAP_SIZE_X;
 extern int MAP_SIZE_Y;
+extern double CHECK_POINT_DENSITY;  // Min: 0; Max: 1
 extern double CHARACTER_UNIT_MOVING_LENGTH;  // Larger than 0
 
 #endif //GAME_CONFIGS_H
