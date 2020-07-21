6f159016bca3dfbfe16156a9056f16b5586b9754
Yang Minshun <33303880+abest2016@users.noreply.github.com>
2020-07-21 10:55:31 +0900

    introduce config file #25 (#38)

---
+++ configs.h
@@ -0,0 +1,15 @@
+//
+// Created by YANG MINSHUN on 7/21/2020.
+//
+
+#ifndef GAME_CONFIGS_H
+#define GAME_CONFIGS_H
+
+extern char *WINDOWS_NAME;
+extern double CHECK_POINT_SENSITIVITY;  // Min: 0; Max: 1
+extern int LENGTH_OF_MAP_BLOCK;
+extern int MAP_SIZE_X;
+extern int MAP_SIZE_Y;
+extern double CHARACTER_UNIT_MOVING_LENGTH;  // Larger than 0
+
+#endif //GAME_CONFIGS_H
