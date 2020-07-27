976e6b5235e7678f5d596a7071aeb9db66746693
tomoino <55827264+tomoino@users.noreply.github.com>
2020-07-27 22:08:09 +0900

    [update]wall(#13) (#57)
    * [update]wall
    
    壁を棒倒し法により自動生成。gameBoardの生成および描画の範囲がgroundと異なっていたため修正。
    
    * [fix]wall
    
    敵位置によって詰まないように変更。棒倒し法から外周の壁を取り除いた。
    
    Co-authored-by: Yang Minshun <yangminshun@abestswc.com>

--- configs.c
+++ configs.c
@@ -1,7 +1,7 @@
 char *WINDOWS_NAME = "Maze Game";
 double CHECK_POINT_SENSITIVITY = 0.3;  // Min: 0; Max: 1
 int LENGTH_OF_MAP_BLOCK = 1;
-int MAP_SIZE_X = 10;
-int MAP_SIZE_Y = 8;
+int MAP_SIZE_X = 11; // 壁生成の都合上奇数が良い
+int MAP_SIZE_Y = 9; // 壁生成の都合上奇数が良い
 double CHECK_POINT_DENSITY = 0.2;  // Min: 0; Max: 1
 double CHARACTER_UNIT_MOVING_LENGTH = 0.1;  // Larger than 0
