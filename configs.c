9d86c9185a5f77d2bf37abc84276c5fe0f61c1a8
uman9021 <67935485+uman9021@users.noreply.github.com>
2020-07-27 17:04:16 +0900

    Update configs.c

--- configs.c
+++ configs.c
@@ -3,5 +3,5 @@ double CHECK_POINT_SENSITIVITY = 0.3;  // Min: 0; Max: 1
 int LENGTH_OF_MAP_BLOCK = 1;
 int MAP_SIZE_X = 10;
 int MAP_SIZE_Y = 8;
-double CHECK_POINT_DENSITY = 0.2;  // Min: 0; Max: 1
+double CHECK_POINT_DENSITY = 0.1;  // Min: 0; Max: 1
 double CHARACTER_UNIT_MOVING_LENGTH = 0.1;  // Larger than 0
